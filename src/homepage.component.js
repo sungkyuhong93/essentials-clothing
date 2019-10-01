e5ec73c1ebd6455d263be6aa6213030bd25b56e3
Sung Kyu Hong <sungkyuhong93@gmail.com>
2019-09-30 22:59:07 -0400

    first-commit

---
+++ src/homepage.component.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="homepage-menu">
+      <div className="homepage-menu-item">hats</div>
+      <div className="homepage-menu-item">hats</div>
+      <div className="homepage-menu-item">hats</div>
+      <div className="homepage-menu-item">hats</div>
+      <div className="homepage-menu-item">hats</div>
+      <div className="homepage-menu-item">hats</div>
+    </div>
+  </div>
+);
+
+export default HomePage;
